855a969ed61b4412a2b10019c8c4abebea8a2da1
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-12-11 11:46:53 +0100

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Alice")
+	bc.AddBlock("Send 2 BTC to Alice")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+
+}
